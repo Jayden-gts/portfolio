cf9c584eee9889293f9ceefc9f918107bfdc1338
Jayden-gts <otesjayden@live.com>
2026-03-03 17:22:25 -0500

    project created

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect, useRef } from 'react'
+import './App.css'
+import p5 from 'p5';
+
+function App() {
+  
+  
+
+  return (
+    <>
+      <div>
+        <h1>Jayden Seto</h1>
+        <p>Student | Computer Science Major | Aspiring Software Engineer</p>
+      </div>
+    </>
+  )
+}
+
+export default App
